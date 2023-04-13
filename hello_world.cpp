2d2f1e98ae14473e053faca1f34a61329ed2664b
Wenir04 <113133600+Wenir04@users.noreply.github.com>
2023-04-13 04:19:43 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,6 +2,6 @@
 #include <string>
 int main(){
 	string name;
-	std::cin >> name; \\Input user name
+	std::cin >> name; \\ Введите имя пользователя
 	std::cout << "Hello world from"<< name << std::endl;
 }
